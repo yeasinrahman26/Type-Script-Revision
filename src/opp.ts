c425407386996ac23b0f380171ac39071d87fdc4
yeasinrahman26 <yeasinrahmansafa@gmail.com>
2026-07-30 21:02:04 +0600

    opp reload

--- src/opp.ts
+++ src/opp.ts
@@ -16,30 +16,72 @@
 // const masalaChai = new Chai("ginger");
 // masalaChai.name = "ginger";
 
+//access modifier
 
-//access modifier 
+class Chai {
+  public flavor: string = "Ginger";
+  private secretIngredients = "cardamom";
 
-class Chai{
-    public flavor: string = "Ginger"
-    private secretIngredients="cardamom"
-
-    reveal(){
-        return this.secretIngredients // ok
-    }
-  
+  reveal() {
+    return this.secretIngredients; // ok
+  }
 }
 
-class shop{
-      protected shopName= 'chai corner'
+class shop {
+  protected shopName = "chai corner";
 }
 
-class Branch extends shop{
-    getName(){
-        return this.shopName
-    }
+class Branch extends shop {
+  getName() {
+    return this.shopName;
+  }
 }
 
 // const c = new Chai()
 // c.reveal()
 
-new Branch().getName
+new Branch().getName;
+
+class Wallet {
+  #balance = 100;
+  getBalance() {
+    return this.#balance;
+  }
+}
+
+const w = new Wallet();
+
+class Cup {
+  readonly capacity: number = 200;
+
+  constructor(capacity: number) {
+    this.capacity = capacity;
+  }
+}
+
+// getter and setters
+class ModernChai {
+  private _sugar = 2;
+
+  get sugar() {
+    return this._sugar;
+  }
+
+  set sugar(value: number) {
+    if (value > 5) throw new Error("too Sweet");
+    this._sugar = value;
+  }
+}
+
+// uses of getter and setters
+
+const c = new ModernChai();
+c.sugar = 3;
+
+class EKChai{
+    static shopName= "chaiCode Cafe"
+    constructor(public flavor:string){}
+}
+console.log(EKChai.shopName) 
+
+// last time stamp  3 hour 15 m 
